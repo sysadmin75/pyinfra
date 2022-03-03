391b6de2d33a8c522ff5afdd1c2461e82d7d3c55
Nick Mills-Barrett <nick@fizzadar.com>
2022-03-03 09:02:15 +0000

    Add build requirements group.

--- setup.py
+++ setup.py
@@ -56,7 +56,12 @@ DOCS_REQUIRES = (
     'docutils==0.17.1',
 )
 
-DEV_REQUIRES = TEST_REQUIRES + DOCS_REQUIRES + (
+
+BUILD_REQUIRES = (
+    'pyinstaller==4.9',
+)
+
+DEV_REQUIRES = TEST_REQUIRES + DOCS_REQUIRES + BUILD_REQUIRES + (
     # Releasing
     'wheel',
     'twine',
@@ -126,6 +131,7 @@ if __name__ == '__main__':
             'docs': DOCS_REQUIRES,
             'dev': DEV_REQUIRES,
             'ansible': ANSIBLE_REQUIRES,
+            'build': BUILD_REQUIRES,
         },
         include_package_data=True,
         classifiers=[
