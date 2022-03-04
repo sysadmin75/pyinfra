0749928e280660f894c1064349f22461873f414b
Nick Mills-Barrett <nick@fizzadar.com>
2022-03-04 09:00:36 +0000

    Strip out as many files from the binary as possible.
    In theory this should yield a startup speed improvement.

--- pyinfra/connectors/__init__.py
+++ pyinfra/connectors/__init__.py
@@ -1,10 +1,31 @@
+import sys
+from importlib import import_module
+
 import pkg_resources
 
-# Connectors that handle generation of inventories
-ALL_CONNECTORS = {
-    entrypoint.name: entrypoint.load()
-    for entrypoint in pkg_resources.iter_entry_points('pyinfra.connectors')
-}
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    # NOTE: this is a stupid workaround
+    # See: https://github.com/pyinstaller/pyinstaller/issues/3050
+    ALL_CONNECTORS = {
+        name: import_module(f'pyinfra.connectors.{name}')
+        for name in (
+            'ansible',
+            'chroot',
+            'docker',
+            'local',
+            'mech',
+            'ssh',
+            'dockerssh',
+            'vagrant',
+            'winrm',
+        )
+    }
+else:
+    ALL_CONNECTORS = {
+        entrypoint.name: entrypoint.load()
+        for entrypoint in pkg_resources.iter_entry_points('pyinfra.connectors')
+    }
 
 
 # Connectors that handle execution of pyinfra operations
